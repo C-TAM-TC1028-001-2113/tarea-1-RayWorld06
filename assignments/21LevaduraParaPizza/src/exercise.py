d22a922773dcd951335adb6da30da7ef05f605b3
DRHR <a01412788@tec.mx>
2021-08-30 10:33:12 -0500

    TAREA LEVADURAS

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,6 +1,11 @@
+# coding=utf-8
 def main():
     # escribe tu código abajo de esta línea
-    pass
+
+    gramosharina = float(input("Dame la harina en gramos: "))
+    calculo = (gramosharina) * (0.050)
+    calculo = round(calculo,1)
+    print("Necesitas estos gramos de levadura:", calculo)
 
 
 if __name__ == '__main__':

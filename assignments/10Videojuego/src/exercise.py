fb1bf3b179390948cf337cfc2f0bcef47c3e1cce
DRHR <a01412788@tec.mx>
2021-08-30 21:14:42 -0500

    TAREA VIDEOJUEGOS

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,11 @@
+# coding=utf-8
 def main():
     # escribe tu código abajo de esta línea
-    pass
+
+    juegos_nuevos = int(input("Dame la cantidad de juegos nuevos: "))
+    juegos_viejos = int(input("Dame la cantidad de juegos viejos: "))
+    total = (juegos_nuevos * 1000) + (juegos_viejos * 350)
+    print("El total de la compra es:", total)
 
 
 if __name__ == '__main__':
